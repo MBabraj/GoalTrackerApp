e2140e451d57ca743fb3dd33db959cef1dc21f59
Jedretti2000 <jedrzejsarna9@gmail.com>
2017-10-29 00:18:49 +0200

    Add button

--- app/src/main/java/com/example/misia/goaltracker/MainActivity.java
+++ app/src/main/java/com/example/misia/goaltracker/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.misia.goaltracker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button=findViewById(R.id.id_przycisk);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Log.d("wprowadz","test");
+
+            }
+
+        });
     }
 }
